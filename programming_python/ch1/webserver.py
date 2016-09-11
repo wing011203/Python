45473e92ae05bbcfde480d11a4280c3b56a74eca
wing011203 <wing011203@163.com>
2016-09-11 17:07:56 +0800

    <Programming Python 4th Edition> ch1

---
+++ programming_python/ch1/webserver.py
@@ -0,0 +1,10 @@
+import os, sys
+from http.server import HTTPServer, CGIHTTPRequestHandler
+
+webdir = '.'
+port = 80
+
+os.chdir(webdir)
+srvraddr = ('', port)
+srvrobj = HTTPServer(srvraddr, CGIHTTPRequestHandler)
+srvrobj.serve_forever()
